fcaf9bbf893ac076976423c2f9d1231afeeb8132
RAJ KUMAR PATEL <raj.kr.patel@hotmail.com>
2026-02-24 02:04:21 +0530

    reverse linked link

---
+++ DSA/src/linkedlist/ReverseLinkedList.java
@@ -0,0 +1,21 @@
+package linkedlist;
+
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+
+        ListNode previous = null;
+        ListNode current = head;
+
+        while(current != null){
+            ListNode temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+
+        return previous;
+
+    }
+
+}
