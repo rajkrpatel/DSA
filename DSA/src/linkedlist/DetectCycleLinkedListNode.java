71b36854263d5e9759f1e8dd635aef87a7668320
RAJ KUMAR PATEL <raj.kr.patel@hotmail.com>
2026-02-24 01:21:52 +0530

    detect cycle linked list node

---
+++ DSA/src/linkedlist/DetectCycleLinkedListNode.java
@@ -0,0 +1,27 @@
+package linkedlist;
+
+public class DetectCycleLinkedListNode {
+
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast !=null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(slow == fast){
+                while (slow != head){
+                    slow = slow.next;
+                    head = head.next;
+                }
+
+                return slow;
+            }
+        }
+
+        return null;
+
+    }
+}
