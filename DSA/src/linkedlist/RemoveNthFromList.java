d134a8f58ef875043cdde892be45ab4062da47ea
RAJ KUMAR PATEL <raj.kr.patel@hotmail.com>
2026-02-25 14:51:09 +0530

    Remove nth element from last in linked list

---
+++ DSA/src/linkedlist/RemoveNthFromList.java
@@ -0,0 +1,23 @@
+package linkedlist;
+
+public class RemoveNthFromList {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = head;
+
+        for(int i=0; i<n; i++){
+            fast = fast.next;
+        }
+
+        while (fast !=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
